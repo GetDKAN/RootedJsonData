dea7dec9a1b3db66c3c8315266021c385625cb5b
Dan Feder <dan.feder@civicactions.com>
2020-11-10 23:33:38 -0500

    PSR2 coding standards

--- src/RootedJsonData.php
+++ src/RootedJsonData.php
@@ -10,70 +10,76 @@ use JsonPath\JsonObject;
 /**
  * [Description RootedData]
  */
-class RootedJsonData {
+class RootedJsonData
+{
 
-  private $schema;
-  private $data;
+    private $schema;
+    private $data;
 
-  public function __construct(string $json = "{}", string $schema = "{}") {
-    $decoded = json_decode($json);
+    public function __construct(string $json = "{}", string $schema = "{}")
+    {
+        $decoded = json_decode($json);
 
-    if (!isset($decoded)) {
-      throw new \Exception("Fix your JSON");
+        if (!isset($decoded)) {
+            throw new \Exception("Fix your JSON");
+        }
+
+        try {
+            $this->schema = Schema::fromJsonString($schema);
+        } catch (\Exception $e) {
+            throw new \Exception("Fix your Schema");
+        }
+
+        $data = new JsonObject($json, true);
+        if (!self::validate($data, $this->schema)->isValid()) {
+            throw new \Exception("Fix your JSON");
+        }
+
+        $this->data = $data;
     }
 
-    try {
-      $this->schema = Schema::fromJsonString($schema);
-    }
-    catch (\Exception $e) {
-      throw new \Exception("Fix your Schema");
-    }
-
-    $data = new JsonObject($json, TRUE);
-    if (!self::validate($data, $this->schema)->isValid()) {
-      throw new \Exception("Fix your JSON");
+    public static function validate(JsonObject $data, Schema $schema)
+    {
+        $validator = new Validator();
+        $result = $validator->schemaValidation(json_decode("{$data}"), $schema);
+        return $result;
     }
 
-    $this->data = $data;
-  }
-
-  public static function validate(JsonObject $data, Schema $schema) {
-    $validator = new Validator();
-    $result = $validator->schemaValidation(json_decode("{$data}"), $schema);
-    return $result;
-  }
-
-  public function __toString() {
-    return (string) $this->data;
-  }
-
-  public function get($path) {
-    $result = $this->data->get($path);
-    if ($result === FALSE) {
-      throw new \Exception("Property {$path} is not set");
-    }
-    return $result;
-  }
-
-  function __get($jsonPath) {
-      return $this->data->get($jsonPath);
-  }
-
-  public function set($path, $value) {
-    $validationJsonObject = new JsonObject((string) $this->data);
-    $validationJsonObject->set($path, $value);
-
-    $result = self::validate($validationJsonObject, $this->schema);
-    if (!$result->isValid()) {
-      $keywordArgs = $result->getFirstError()->keywordArgs();
-      throw new \Exception("{$path} expects a {$keywordArgs['expected']}");
+    public function __toString()
+    {
+        return (string) $this->data;
     }
 
-    return $this->data->set($path, $value);
-  }
+    public function get($path)
+    {
+        $result = $this->data->get($path);
+        if ($result === false) {
+            throw new \Exception("Property {$path} is not set");
+        }
+        return $result;
+    }
 
-  function __set($jsonPath, $value) {
-    return $this->data->set($jsonPath, $value);
-  }
+    public function __get($jsonPath)
+    {
+        return $this->data->get($jsonPath);
+    }
 
+    public function set($path, $value)
+    {
+        $validationJsonObject = new JsonObject((string) $this->data);
+        $validationJsonObject->set($path, $value);
+
+        $result = self::validate($validationJsonObject, $this->schema);
+        if (!$result->isValid()) {
+            $keywordArgs = $result->getFirstError()->keywordArgs();
+            throw new \Exception("{$path} expects a {$keywordArgs['expected']}");
+        }
+
+        return $this->data->set($path, $value);
+    }
+
+    public function __set($jsonPath, $value)
+    {
+        return $this->data->set($jsonPath, $value);
+    }
 }
--- tests/RootedDatatTest.php
+++ tests/RootedDatatTest.php
@@ -3,122 +3,112 @@
 
 namespace RootedDataTest;
 
-
 use PHPUnit\Framework\TestCase;
 use RootedData\RootedJsonData;
 
-class RootedDatatTest extends TestCase {
+class RootedJsonDataTest extends TestCase
+{
+    public function testSeamlessExperience()
+    {
+        $data = new RootedJsonData();
+        $data->set("$.title", "Hello");
+        $this->assertEquals('{"title":"Hello"}', "{$data}");
 
-  /**
+        $data->set("$.publisher.name", "Frank");
 
-  -- We want an interface/API to modify the values of properties of a data object without having to worry about the schema constraints being violated.
-  -- The interface should make access to the serialized json or php structure available.
-  -- All data properties are private, modification must be made through setter methods/function that validate any modifications.
-  -- Allow bulk modifications of the RootedData.
+        $this->assertEquals('{"title":"Hello","publisher":{"name":"Frank"}}', (string) $data);
+    }
 
-  Consolidate our business logic around this new way of working with JSON data.
-   * We are assuming that we start with a json object.
-   * Can we use the schema to inform non-existing structure?
-   * Pass schema to sub rooteddata objects.
-   * sub rooteddata objects can not set their own schema
-   * we should fail when the schema is violated.
-   * Test array manipulations
-   */
+    public function testMagicGetterAndSetter()
+    {
+        $data = new RootedJsonData();
+        $data->{"$.title"} = "Hello";
+        $this->assertEquals('{"title":"Hello"}', "{$data}");
+        $this->assertEquals("Hello", $data->{"$.title"});
+    }
 
-  public function testSeamlessExperience() {
+    public function testAccessToNonExistentProperties()
+    {
+        $this->expectExceptionMessage("Property $.city is not set");
+        $data = new RootedJsonData();
+        $city = $data->get("$.city");
+    }
 
-    // we want a seemless experience between json strings and their structure they represent.
-    $data = new RootedJsonData();
-    $data->set("$.title", "Hello");
-    $this->assertEquals('{"title":"Hello"}', "{$data}");
+    public function testJsonFormat()
+    {
+      // We want our data to keep its integrity in the in-betweens: From input to output.
+        $this->expectExceptionMessage("Fix your JSON");
+        $json = "{";
+        new RootedJsonData($json);
+    }
 
-    $data->set("$.publisher.name", "Frank");
-
-    $this->assertEquals('{"title":"Hello","publisher":{"name":"Frank"}}', (string) $data);
-  }
-
-  public function testMagicGetterAndSetter() {
-    $data = new RootedJsonData();
-    $data->{"$.title"} = "Hello";
-    $this->assertEquals('{"title":"Hello"}', "{$data}");
-    $this->assertEquals("Hello", $data->{"$.title"});
-  }
-
-  public function testAccessToNonExistentProperties() {
-    $this->expectExceptionMessage("Property $.city is not set");
-    $data = new RootedJsonData();
-    $city = $data->get("$.city");
-  }
-
-  public function testJsonFormat() {
-    // We want our data to keep its integrity in the in-betweens: From input to output.
-    $this->expectExceptionMessage("Fix your JSON");
-    $json = "{";
-    new RootedJsonData($json);
-  }
-
-  public function testJsonIntegrityFailure() {
-    $this->expectExceptionMessage("Fix your JSON");
-    $json = '{"number":"hello"}';
-    $schema = '{
+    public function testJsonIntegrityFailure()
+    {
+        $this->expectExceptionMessage("Fix your JSON");
+        $json = '{"number":"hello"}';
+        $schema = '{
       "type": "object",
       "properties": {
         "number":      { "type": "number" }
       }
     }';
-    new RootedJsonData($json, $schema);
-  }
+        new RootedJsonData($json, $schema);
+    }
 
-  public function testSchemaIntegrity() {
-    $this->expectExceptionMessage("Fix your Schema");
-    $json = '{"number":"hello"}';
-    $schema = '{
+    public function testSchemaIntegrity()
+    {
+        $this->expectExceptionMessage("Fix your Schema");
+        $json = '{"number":"hello"}';
+        $schema = '{
       "type": "object",
       "properties": {
         "number":      { "type": "number" }
       }';
-    new RootedJsonData($json, $schema);
-  }
+        new RootedJsonData($json, $schema);
+    }
 
-  public function testJsonIntegrity() {
-    $json = '{"number":51}';
-    $schema = '{
+    public function testJsonIntegrity()
+    {
+        $json = '{"number":51}';
+        $schema = '{
       "type": "object",
       "properties": {
         "number":      { "type": "number" }
       }
     }';
-    $data = new RootedJsonData($json, $schema);
-    $this->assertEquals($json, "{$data}");
-  }
+        $data = new RootedJsonData($json, $schema);
+        $this->assertEquals($json, "{$data}");
+    }
 
-  public function testJsonIntegrityFailureAfterChange() {
-    $this->expectExceptionMessage("\$.number expects a number");
+    public function testJsonIntegrityFailureAfterChange()
+    {
+        $this->expectExceptionMessage("\$.number expects a number");
 
-    $json = '{"number":51}';
-    $schema = '{
+        $json = '{"number":51}';
+        $schema = '{
       "type": "object",
       "properties": {
         "number":      { "type": "number" }
       }
     }';
-    $data = new RootedJsonData($json, $schema);
-    $this->assertEquals($json, "{$data}");
+        $data = new RootedJsonData($json, $schema);
+        $this->assertEquals($json, "{$data}");
 
-    $data->set("$.number", "Alice");
-  }
+        $data->set("$.number", "Alice");
+    }
 
-  public function testJsonPathGetter() {
-    $json = '{"container":{"number":51}}';
-    $data = new RootedJsonData($json);
-    $this->assertEquals(51, $data->get("$.container.number"));
-  }
+    public function testJsonPathGetter()
+    {
+        $json = '{"container":{"number":51}}';
+        $data = new RootedJsonData($json);
+        $this->assertEquals(51, $data->get("$.container.number"));
+    }
 
-  public function testJsonPathSetter() {
-    $json = '{"container":{"number":51}}';
-    $data = new RootedJsonData($json);
-    $data->set("$.container.number", 52);
-    $this->assertEquals(52, $data->get("$.container.number"));
-  }
-
+    public function testJsonPathSetter()
+    {
+        $json = '{"container":{"number":51}}';
+        $data = new RootedJsonData($json);
+        $data->set("$.container.number", 52);
+        $this->assertEquals(52, $data->get("$.container.number"));
+    }
 }
