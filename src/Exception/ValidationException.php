aeb136524b40aba97ab3338e35cae66d0675d10a
Dan Feder <dan.feder@civicactions.com>
2020-11-12 16:37:56 -0500

    Documentation

--- src/Exception/ValidationException.php
+++ src/Exception/ValidationException.php
@@ -4,16 +4,38 @@ namespace RootedData\Exception;
 
 use Opis\JsonSchema\ValidationResult;
 
+/**
+ * Exception class to throw for RootedJsonData objects that fail validation.
+ *
+ * Passes along the Opis Json Schema validation result.
+ */
 class ValidationException extends \InvalidArgumentException
 {
+    /**
+     * Validation result report.
+     *
+     * @var Opis\JsonSchema\ValidationResult
+     */
     private $validationResult;
 
-    public function __construct($message, ValidationResult $validationResult)
+    /**
+     * @param string $message
+     *   Exception message.
+     * @param ValidationResult $validationResult
+     *   Validation result report.
+     */
+    public function __construct(string $message, ValidationResult $validationResult)
     {
         $this->validationResult = $validationResult;
         parent::__construct($message);
     }
 
+    /**
+     * Get the validation result object.
+     *
+     * @return Opis\JsonSchema\ValidationResult
+     *   Validation result report.
+     */
     public function getResult()
     {
         return $this->validationResult;
