b87b0462eecd7e50727aedf90322cebafdf4d98a
Dan Feder <dan.feder@civicactions.com>
2026-05-22 10:21:50 -0400

    Switch InvalidSchemaException to extend RuntimeException

--- src/Exception/InvalidSchemaException.php
+++ src/Exception/InvalidSchemaException.php
@@ -6,9 +6,9 @@ namespace RootedData\Exception;
  * Thrown when a JSON Schema string passed to RootedJsonData is malformed.
  *
  * Replaces the v1 dependency on Opis\JsonSchema\Exception\InvalidSchemaException,
- * which was removed in opis/json-schema 2.x. Extends \InvalidArgumentException
+ * which was removed in opis/json-schema 2.x. Extends \RuntimeException
  * to preserve catch-block compatibility for callers that did not import the opis class.
  */
-class InvalidSchemaException extends \InvalidArgumentException
+class InvalidSchemaException extends \RuntimeException
 {
 }
